f55b7fbb7c13645d3590e3d3e7175bb6b4abbdc4
故里 <gj2499151476@gmail.com>
2023-10-23 16:41:15 +0800

    构建bat

---
+++ bin/autoDeploy.bat
@@ -0,0 +1,9 @@
+call pnpm docs:build
+cd docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m "auto construct blog"
+
+git push -f https://github.com/taotaoguli/taotaoguli.github.io.git master:gh-pages
+
